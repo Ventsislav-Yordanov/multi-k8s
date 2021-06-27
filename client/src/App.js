feff8d454aa6b1db3efbd3d35fb5118c0b133500
Ventsislav Yordanov <venciityjordanov@gmail.com>
2021-06-27 21:52:41 +0300

    Updated the home page

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,8 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 3
+            MY first deployed app! :)))))
+            WOHOOOOOOOOOOOOOOOOOOOOO!
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
